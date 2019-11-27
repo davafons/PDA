e641164782d4a8034f9d13da4f958f105feb79fa
David Afonso <davafons@gmail.com>
2019-11-27 19:36:39 +0000

    :sparkles: Show errors on file building

--- src/core/pdabuilder.cpp
+++ src/core/pdabuilder.cpp
@@ -24,44 +24,58 @@ Pda PdaBuilder::fromFile(const std::string& file_path) {
 
   Pda pda;
 
-  pda.addStates(Utils::nextLine(file_stream));
+  std::string line;
+  try {
+    line = Utils::nextLine(file_stream);
+    pda.addStates(line);
 
-  pda.tapeAlphabet().addSymbols(Utils::nextLine(file_stream));
-  pda.stackAlphabet().addSymbols(Utils::nextLine(file_stream));
+    line = Utils::nextLine(file_stream);
+    pda.tapeAlphabet().addSymbols(line);
 
-  pda.setStartState(Utils::nextLine(file_stream));
-  pda.setStackStartSymbol(Utils::nextLine(file_stream));
+    line = Utils::nextLine(file_stream);
+    pda.stackAlphabet().addSymbols(line);
 
-  // At this point, there's a line that is hard to parse:
-  // On a "Final state PDA" file, this line says which are the final states.
-  // On a "Empty stack PDA" file, the "final states" line isn't present, so this line is
-  // alredy the first transtion.
+    line = Utils::nextLine(file_stream);
+    pda.setStartState(line);
 
-  // So, we check if the symbols read on this line are states or not in order to guess
-  // the type of the PDA.
-  std::string conflictive_line = Utils::nextLine(file_stream);
-  std::stringstream line_stream(conflictive_line);
-  std::string token;
-  bool is_states_line = true;
-  while (line_stream >> token && is_states_line) {
-    if (!pda.hasState(token)) {
-      is_states_line = false;
+    line = Utils::nextLine(file_stream);
+    pda.setStackStartSymbol(line);
+
+    // At this point, there's a line that is hard to parse:
+    // On a "Final state PDA" file, this line says which are the final states.
+    // On a "Empty stack PDA" file, the "final states" line isn't present, so this line
+    // is alredy the first transtion.
+
+    // So, we check if the symbols read on this line are states or not in order to guess
+    // the type of the PDA.
+    line = Utils::nextLine(file_stream);
+    std::stringstream line_stream(line);
+    std::string token;
+    bool is_states_line = true;
+    while (line_stream >> token && is_states_line) {
+      if (!pda.hasState(token)) {
+        is_states_line = false;
+      }
     }
-  }
 
-  // Set the PDA type
-  if (is_states_line) {
-    pda.setPdaType(Pda::Type::FinalState);
-    pda.setFinalStates(conflictive_line);
-  } else {
-    pda.setPdaType(Pda::Type::EmptyStack);
-    pda.addTransition(conflictive_line);
-  }
+    // Set the PDA type
+    if (is_states_line) {
+      pda.setPdaType(Pda::Type::FinalState);
+      pda.setFinalStates(line);
+    } else {
+      pda.setPdaType(Pda::Type::EmptyStack);
+      pda.addTransition(line);
+    }
 
-  // Read transitions
-  std::string transition_line;
-  while (std::getline(file_stream, transition_line)) {
-    pda.addTransition(transition_line);
+    // Read transitions
+    while (std::getline(file_stream, line)) {
+      pda.addTransition(line);
+    }
+  } catch (const std::exception& e) {
+    std::string message = "Error on Line: " + line + "\n";
+    message += std::string("||") + e.what() + "\n";
+
+    throw std::runtime_error(message);
   }
 
   return pda;
